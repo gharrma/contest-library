b191df067f5db81edc5fce7249b108ce94cda342
Matthew Gharrity <mjg355@cornell.edu>
2017-01-14 01:18:41 -0600

    Add bipartite matching

---
+++ algorithms/bipartite-matching.cpp
@@ -0,0 +1,56 @@
+/*
+ * Bipartite matching.
+ */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct bipartite_graph {
+    int a, b;
+    vector<vector<int>> adj;
+    vector<int> visit, match;
+
+    bipartite_graph(int a, int b)
+        : a(a), b(b), adj(a+b), visit(a+b, -1), match(a+b, -1) {}
+
+    void edge(int i, int j) {
+        adj[i].push_back(a+j);
+        adj[a+j].push_back(i);
+    }
+
+    bool augment(int n, int run) {
+        if (visit[n] == run)
+            return false;
+        visit[n] = run;
+        for (int c : adj[n]) {
+            if (match[c] == -1 || augment(match[c], run)) {
+                match[n] = c, match[c] = n;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int matching() {
+        int ret = 0;
+        for (int i = 0; i < a; ++i)
+            ret += augment(i, i);
+        return ret;
+    }
+};
+
+int main() {
+    bipartite_graph g(4, 4);
+    g.edge(0, 0);
+    g.edge(0, 1);
+    g.edge(1, 0);
+    g.edge(1, 1);
+    g.edge(2, 0);
+    g.edge(2, 1);
+    g.edge(3, 1);
+    g.edge(3, 2);
+    g.edge(3, 3);
+    cout << g.matching() << endl; // 3
+    return 0;
+}

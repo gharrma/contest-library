0aee2635c62adba33ddd55562f4d8e7cc08b3d54
Matthew Gharrity <mjg355@cornell.edu>
2017-01-15 14:00:25 -0600

    Add loops to template

--- template.cpp
+++ template.cpp
@@ -11,6 +11,11 @@ using namespace std;
     cout << endl)
 #define WATCHC(v) TRACE({cout << #v" = "; WATCHR(v.begin(), v.end());})
 
+#define FU(i, a, b) for (remove_const_t<remove_reference_t<decltype(b)>> i = (a); i < (b); ++i)
+#define fu(i, b) FU(i, 0, b)
+#define FD(i, a, b) for (remove_const_t<remove_reference_t<decltype(b)>> i = (b) - 1; i >= (a); --i)
+#define fd(i, b) FD(i, 0, b)
+
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 

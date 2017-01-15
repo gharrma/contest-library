c2b9a87eead4662dbbb431e947dd77de4b0b5fa7
Matthew Gharrity <mjg355@cornell.edu>
2017-01-15 15:03:11 -0600

    Improve geometry style

--- math/geometry.cpp
+++ math/geometry.cpp
@@ -7,22 +7,22 @@ struct point {
 
     explicit point<T>(T x = 0, T y = 0): x(x), y(y) {}
 
-    point operator +(point q) const { return point(x + q.x, y + q.y); }
-    point operator -(point q) const { return point(x - q.x, y - q.y); }
-    point operator *(T t)     const { return point(x * t, y * t); }
-    point operator /(T t)     const { return point(x / t, y / t); }
+    point operator+(point q) const { return point(x + q.x, y + q.y); }
+    point operator-(point q) const { return point(x - q.x, y - q.y); }
+    point operator*(T t)     const { return point(x * t, y * t); }
+    point operator/(T t)     const { return point(x / t, y / t); }
 
     // Dot product.
-    T operator *(point q) const { return x * q.x + y * q.y; }
+    T operator*(point q) const { return x * q.x + y * q.y; }
 
     // "Cross" product (signed area of the quadrilateral).
-    T operator %(point q) const { return x * q.y - y * q.x; }
+    T operator%(point q) const { return x * q.y - y * q.x; }
 
-    bool operator ==(point q) const
+    bool operator==(point q) const
         { return std::tie(x, y) == std::tie(q.x, q.y); }
-    bool operator !=(point q) const
+    bool operator!=(point q) const
         { return std::tie(x, y) != std::tie(q.x, q.y); }
-    bool operator < (point q) const
+    bool operator<(point q) const
         { return std::tie(x, y) < std::tie(q.x, q.y); }
 };
 

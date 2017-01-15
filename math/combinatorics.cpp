37b31805bdbf7ca85cf8941adcb9fdef0872dce8
Matthew Gharrity <mjg355@cornell.edu>
2017-01-15 14:11:48 -0600

    Add ncrf to combinatorics

--- math/combinatorics.cpp
+++ math/combinatorics.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
-typedef unsigned long long ull;
-constexpr ull mod = 1e9 + 7;
+using ll = long long;
+constexpr ll mod = 1e9 + 7;
 
-constexpr ull fact_max = 1e6;
-vector<ull> fact(fact_max + 1);
+constexpr size_t max_fact = 1e6;
+ll fact[max_fact + 1];
 
-ull pow(ull b, ull e) {
+constexpr size_t max_ncrf = 1e3;
+double ncrf[max_ncrf + 1][max_ncrf + 1];
+
+ll pow(ll b, ll e) {
     if (e == 0) return 1;
-    ull rec = pow(b, e / 2);
+    ll rec = pow(b, e / 2);
     if (e % 2 == 0) return rec * rec % mod;
     else return rec * rec % mod * b % mod;
 }
 
-ull inverse(ull n) {
+ll inverse(ll n) {
     return pow(n, mod - 2);
 }
 
-ull ncr(ull n, ull r) {
+ll ncr(ll n, ll r) {
     return n < r ? 0 : fact[n] * inverse(fact[r] * fact[n - r] % mod) % mod;
 }
 
 int main() {
     fact[0] = fact[1] = 1;
-    for (ull i = 2; i <= fact_max; ++i) {
+    for (ll i = 2; i <= max_fact; ++i) {
         fact[i] = fact[i-1] * i % mod;
+    }
+    for (size_t i = 0; i <= max_ncrf; ++i) {
+        ncrf[i][0] = ncrf[i][i] = 1.;
+        for (size_t j = 1; j < i; ++j)
+            ncrf[i][j] = ncrf[i-1][j-1] + ncrf[i-1][j];
     }
     return 0;
 }

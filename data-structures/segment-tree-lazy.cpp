14444ca7808e87801934301561d382a01b0a114e
Matthew Gharrity <mjg355@cornell.edu>
2017-01-18 14:42:01 -0600

    Improve segment tree usability

--- data-structures/segment-tree-lazy.cpp
+++ data-structures/segment-tree-lazy.cpp
@@ -4,12 +4,16 @@
  * Root is 1. Parent of node i is i/2. Children of node i are 2*i and 2*i+1.
  * Leaf nodes are never lazy.
  *
+ * Optimized for two types of updates:
+ * - Set all elements in a range to a constant group element
+ * - Operate over a range of elements using a constant group element
+ *
  * s := size (number of leaves)
  * h := height
  * v := underlying array
  * lazy := unpropagated updates
- * f := group operation (not necessarily commutative)
  * id := group identity
+ * op := group operation (not necessarily commutative)
  * d := distance from the leaves
  * u := an update to a value t at distance d, supporting composition
  * push := push laziness down along a path from the root to leaf node i
@@ -23,30 +27,31 @@
 #include <cassert>
 using namespace std;
 
-template <typename T, typename Op, typename Update>
+template <typename Group>
 struct seg_tree_lazy {
+    using T = typename Group::T;
+    using Update = typename Group::update;
     size_t s, h;
     vector<T> v;
     vector<Update> lazy;
-    Op f;
 
-    seg_tree_lazy<T, Op, Update>(size_t n) {
+    seg_tree_lazy<Group>(size_t n) {
         for (s = 1, h = 1; s < n; )
             s <<= 1, ++h;
-        v.resize(2*s, T(Op::id));
+        v.resize(2*s, T(Group::id));
         lazy.resize(s);
     }
 
     void apply(size_t i, size_t d, const Update& u) {
-        v[i] = u(v[i], d);
+        v[i] = u.apply(v[i], d);
         if (i < s)
-            lazy[i] = lazy[i] + u;
+            lazy[i] = lazy[i].compose(u);
     }
 
     void push(size_t i) {
         for (size_t d = h; d > 0; --d) {
             size_t l = i >> d;
-            if (lazy[l]) {
+            if (!lazy[l].isNoop()) {
                 apply(2*l,   d-1, lazy[l]);
                 apply(2*l+1, d-1, lazy[l]);
                 lazy[l] = Update();
@@ -57,8 +62,8 @@ struct seg_tree_lazy {
     void pull(size_t i) {
         for (size_t d = 1; d <= h; ++d) {
             size_t l = i >> d;
-            T combined = f(v[2*l], v[2*l+1]);
-            v[l] = lazy[l](combined, d);
+            T combined = Group::op(v[2*l], v[2*l+1]);
+            v[l] = lazy[l].apply(combined, d);
         }
     }
 
@@ -75,45 +80,48 @@ struct seg_tree_lazy {
     T query(size_t i, size_t j) {
         i += s, j += s;
         push(i), push(j);
-        T l = Op::id, r = Op::id;
+        T l = Group::id, r = Group::id;
         for (; i <= j; i /= 2, j /= 2) {
-            if (i % 2 == 1) l = f(l, v[i++]);
-            if (j % 2 == 0) r = f(v[j--], r);
+            if (i % 2 == 1) l = Group::op(l, v[i++]);
+            if (j % 2 == 0) r = Group::op(v[j--], r);
         }
-        return f(l, r);
+        return Group::op(l, r);
     }
 };
 
-struct op {
-    static constexpr int id = 0;
-    int operator()(int a, int b) { return a + b; }
+struct group {
+    using T = int;
+    static constexpr T id = 0;
+    static T op(T a, T b) { return a + b; }
+    static T exp(T x, size_t e) { return x * e; }
+    struct update;
 };
 
-struct update {
-    enum kind { kNoop, kIncrease, kSet };
+struct group::update {
+    enum kind { kNoop, kSet, kOperate };
     kind k;
-    int x;
+    T x;
 
-    update(kind k = kNoop, int x = 0): k(k), x(x) {}
+    update(kind k = kNoop, T x = id): k(k), x(x) {}
 
-    explicit operator bool() const { return k != kNoop; }
+    bool isNoop() const { return k == kNoop; }
 
-    int operator()(int n, size_t d) const {
+    T apply(T n, size_t d) const {
         switch (k) {
-            case kNoop:     return n;
-            case kIncrease: return n + x * (1 << d);
-            case kSet:      return x * (1 << d);
+            case kNoop:    return n;
+            case kSet:     return exp(x, 1u << d);
+            case kOperate: return op(n, exp(x, 1u << d));
         }
     }
 
-    update operator+(const update& other) const {
+    update compose(const update& other) const {
         if (k == kNoop || other.k == kSet) {
             return other;
         } else if (other.k == kNoop) {
             return *this;
         } else {
-            assert(other.k == kIncrease);
-            return update(k, x + other.x);
+            assert(other.k == kOperate);
+            return update(k, op(x, other.x));
         }
     }
 };
@@ -121,15 +129,17 @@ struct update {
 int main() {
     int n = 100;
     vector<int> v(n);
-    seg_tree_lazy<int, op, update> s(n);
+    seg_tree_lazy<group> s(n);
     for (int t = 0; t < 1000000; ++t) {
         if (rand() % 2) {
             size_t l = rand() % n, r = rand() % n;
             int val = rand() % 100;
-            auto kind = rand() % 2 ? update::kIncrease : update::kSet;
+            auto kind = rand() % 2
+                ? group::update::kOperate
+                : group::update::kSet;
             for (int i = l; i <= r; ++i)
-                v[i] = val + (kind == update::kIncrease ? v[i] : 0);
-            s.update(l, r, update(kind, val));
+                v[i] = val + (kind == group::update::kOperate ? v[i] : 0);
+            s.update(l, r, group::update(kind, val));
         } else {
             size_t l = rand() % n, r = rand() % n;
             if (r < l) swap(l, r);
--- data-structures/segment-tree.cpp
+++ data-structures/segment-tree.cpp
@@ -4,8 +4,8 @@
  * Root is 1. Parent of node i is i/2. Children of node i are 2*i and 2*i+1.
  * s := size (number of leaves)
  * v := underlying array
- * f := group operation (not necessarily commutative)
  * id := group identity
+ * op := group operation (not necessarily commutative)
  */
 #include <iostream>
 #include <algorithm>
@@ -13,45 +13,46 @@
 #include <cassert>
 using namespace std;
 
-template <typename T, typename Op>
+template <typename Group>
 struct seg_tree {
+    using T = typename Group::T;
     size_t s;
     vector<T> v;
-    Op f;
 
-    seg_tree<T, Op>(size_t n) {
+    seg_tree<Group>(size_t n) {
         for (s = 1; s < n; )
             s <<= 1;
-        v.resize(2*s, T(Op::id));
+        v.resize(2*s, T(Group::id));
     }
 
     void update(size_t i, T t) {
         i += s;
         v[i] = t;
         for (i /= 2; i > 0; i /= 2)
-            v[i] = f(v[2*i], v[2*i+1]);
+            v[i] = Group::op(v[2*i], v[2*i+1]);
     }
 
     T query(size_t i, size_t j) {
         i += s, j += s;
-        T l = Op::id, r = Op::id;
+        T l = Group::id, r = Group::id;
         for (; i <= j; i /= 2, j /= 2) {
-            if (i % 2 == 1) l = f(l, v[i++]);
-            if (j % 2 == 0) r = f(v[j--], r);
+            if (i % 2 == 1) l = Group::op(l, v[i++]);
+            if (j % 2 == 0) r = Group::op(v[j--], r);
         }
-        return f(l, r);
+        return Group::op(l, r);
     }
 };
 
-struct op {
-    static constexpr int id = 0;
-    int operator()(int a, int b) { return max(a, b); }
+struct group {
+    using T = int;
+    static constexpr T id = 0;
+    static T op(T a, T b) { return max(a, b); }
 };
 
 int main() {
     int n = 100;
     vector<int> v(n);
-    seg_tree<int, op> s(n);
+    seg_tree<group> s(n);
     for (int t = 0; t < 1000000; ++t) {
         if (rand() % 2) {
             int i = rand() % n, val = rand() % 100;

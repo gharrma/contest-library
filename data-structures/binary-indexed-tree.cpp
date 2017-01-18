38d17096aeb2945ea6b2ddb0471ca9cae8a908db
Matthew Gharrity <mjg355@cornell.edu>
2017-01-18 13:57:34 -0600

    Add lower_bound to binary indexed tree

--- data-structures/binary-indexed-tree.cpp
+++ data-structures/binary-indexed-tree.cpp
@@ -32,22 +32,43 @@ struct bi_tree {
     T range(size_t l, size_t r) {
         return r < l ? 0 : prefix(r) - prefix(l-1);
     }
+
+    size_t lower_bound(T x) {
+        size_t i = 0, mask = 1;
+        while (mask < v.size())
+            mask <<= 1;
+        for (; mask > 0; mask >>= 1)
+            if (i + mask < v.size() && v[i + mask] < x)
+                i += mask, x -= v[i];
+        return i;
+    }
 };
 
 int main() {
     int n = 100;
     vector<int> v(n);
     bi_tree<int> b(n);
+    int total_val = 0;
     for (int t = 0; t < 1000000; ++t) {
-        if (rand() % 2) {
+        int r = rand() % 3;
+        if (r == 0) {
             int i = rand() % n, val = rand() % 100;
             v[i] += val;
             b.increase(i, val);
-        } else {
+            total_val += val;
+        } else if (r == 1) {
             size_t l = rand() % n, r = rand() % n;
             if (r < l) swap(l, r);
             int sum = accumulate(v.begin() + l, v.begin() + r + 1, 0);
             assert(b.range(l, r) == sum);
+        } else {
+            vector<int> p(n);
+            p[0] = v[0];
+            for (int i = 1; i < n; ++i)
+                p[i] = p[i-1] + v[i];
+            int x = rand() % (total_val + total_val / 2 + 1);
+            int idx = distance(p.begin(), lower_bound(p.begin(), p.end(), x));
+            assert(b.lower_bound(x) == idx);
         }
     }
     cout << "All tests passed" << endl;

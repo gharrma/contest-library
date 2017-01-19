347840b8359fa7f10f04adf3bb38761ea9bd4a7c
Matthew Gharrity <mjg355@cornell.edu>
2017-01-18 21:38:03 -0600

    Tweaks to weighted graph

--- algorithms/graph-weighted.cpp
+++ algorithms/graph-weighted.cpp
@@ -9,12 +9,14 @@
  * Dijkstra:
  * - returns distances from node s to every other
  * - negative edge weights not supported
- * - can use `prev` to recover paths
+ *
+ * Bellman-Ford:
+ * - returns distances from node s to every other
+ * - detects negative cycles
  *
  * Floyd-Warshall:
  * - returns distances between every pair of nodes
  * - negative cycles not supported
- * - can use `next` to recover paths
  */
 #include <iostream>
 #include <vector>
@@ -34,7 +36,8 @@ struct graph {
     void arc (int i, int j, T w) { adj[i].emplace_back(j, w); }
     void edge(int i, int j, T w) { arc(i, j, w), arc(j, i, w); }
 
-    vector<T> dijkstra(int s) {
+    pair<vector<T>, vector<T>>
+    dijkstra(int s) {
         vector<T> dist(n, T(infty));
         vector<int> prev(n, -1);
         dist[s] = 0;
@@ -61,10 +64,11 @@ struct graph {
             }
         }
 
-        return dist;
+        return make_pair(dist, prev);
     }
 
-    vector<T> bellman_ford(int s, bool& negative_cycle) {
+    pair<vector<T>, vector<T>>
+    bellman_ford(int s, bool& negative_cycle) {
         negative_cycle = false;
         vector<T> dist(n, T(infty));
         vector<int> prev(n, -1);
@@ -80,10 +84,11 @@ struct graph {
             for (auto& p : adj[x])
                 if (dist[p.first] > dist[x] + p.second)
                     negative_cycle = true;
-        return dist;
+        return make_pair(dist, prev);
     }
 
-    vector<vector<T>> floyd_warshall() {
+    pair<vector<vector<T>>, vector<vector<int>>>
+    floyd_warshall() {
         vector<vector<T>> dist(n, vector<T>(n, T(infty)));
         vector<vector<int>> next(n, vector<int>(n, -1));
         for (int i = 0; i < n; ++i) {
@@ -102,7 +107,7 @@ struct graph {
                         next[i][j] = next[i][k];
                     }
 
-        return dist;
+        return make_pair(dist, next);
     }
 };
 
@@ -112,7 +117,7 @@ int main() {
     g.arc(1, 2, 1);
     g.arc(0, 3, 1);
     g.arc(3, 2, 3);
-    assert(g.dijkstra(0) == vector<int>({0, 2, 3, 1}));
+    assert(g.dijkstra(0).first == vector<int>({0, 2, 3, 1}));
 
     g = graph<int>(3);
     g.edge(0, 1, 3);
@@ -124,15 +129,15 @@ int main() {
 
     int n = 20;
     g = graph<int>(n);
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 10000; ++i) {
         if (rand() % 2) {
             g.arc(rand() % n, rand() % n, rand() % 1000);
         } else {
             int s = rand() % n;
             negative_cycle = false;
-            auto dij = g.dijkstra(s);
-            auto bf = g.bellman_ford(s, negative_cycle);
-            auto fw = g.floyd_warshall();
+            auto dij = g.dijkstra(s).first;
+            auto bf = g.bellman_ford(s, negative_cycle).first;
+            auto fw = g.floyd_warshall().first;
             assert(!negative_cycle);
             for (int i = 0; i < n; ++i)
                 assert(dij[i] == bf[i] && bf[i] == fw[s][i]);

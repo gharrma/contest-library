e263790b5ee7b28dff96797d7b8e7e9f7d7d5d6e
Matthew Gharrity <mjg355@cornell.edu>
2017-01-22 17:30:58 -0500

    Add better tests for suffix array

--- algorithms/suffix-array.cpp
+++ algorithms/suffix-array.cpp
@@ -1,11 +1,14 @@
 /*
  * Suffix array for an arbitrary sequence of values.
- * O(n lg n lg n), but can be reduced to O(n lg n) using counting sort.
+ * Gives the lexicographic ordering of all suffixes of the input sequence,
+ *   enabling quick operations such as longest common prefix (LCP).
+ * O(n lg n lg n) construction, but reducible to O(n lg n) with counting sort.
+ * O(lg n) to find the longest common prefix of two suffixes.
+ *
  * v := the sequence of values (usually a string)
  * log := lg(n) (ceil)
- * dp[k][i] := ordered position of substring i among (2^k)-length substrings.
- * order[i] := the start index of the lexicographically ith lowest suffix
- * lcp := longest common prefix
+ * dp[k][i] := sorted position of substring i among (2^k)-length substrings.
+ * order[i] := the start index of the ith lexicographically lowest suffix
  */
 #include <iostream>
 #include <vector>
@@ -50,18 +53,41 @@ struct suffix_array {
     }
 
     size_t lcp(size_t i, size_t j) {
-        size_t ret = 0;
-        for (size_t pow = log; pow != -1 && i < n && j < n; )
+        size_t ret = 0, pow = log, limit = min(n - i, n - j);
+        while (pow-- > 0 && i < n && j < n)
             if (dp[pow][i] == dp[pow][j])
                 ret += 1 << pow, i += 1 << pow, j += 1 << pow;
-        return ret;
+        return min(ret, limit);
     }
 };
 
 int main() {
-    suffix_array<string> s("bobocel");
-    assert(s.dp.back() == vector<size_t>({0,5,1,6,2,3,4}));
-    assert(s.order == vector<size_t>({0,2,4,5,6,1,3}));
+    suffix_array<string> example("apple");
+    assert(example.dp.back() == vector<size_t>({0,4,3,2,1}));
+    assert(example.order == vector<size_t>({0,4,3,2,1}));
+
+    int n = 1000, iter = 1000;
+    for (int t = 0; t < iter; ++t) {
+        string s(n, '\0');
+        for (int i = 0; i < n; ++i)
+            s[i] = rand() % 26 + 'a';
+
+        vector<string> suffixes(n);
+        for (int i = 0; i < n; ++i)
+            suffixes[i] = s.substr(i);
+        sort(suffixes.begin(), suffixes.end());
+        suffix_array<string> arr(s);
+        for (int i = 0; i < n; ++i)
+            assert(n - arr.order[i] == suffixes[i].size());
+
+        int i = rand() % n, j = rand() % n;
+        int len = 0;
+        while (i < n && j < n && s[i] == s[j])
+            ++len, ++i, ++j;
+        i -= len, j -= len;
+        assert(arr.lcp(i, j) == len);
+    }
+
     cout << "All tests passed" << endl;
     return 0;
 }

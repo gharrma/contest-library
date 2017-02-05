688f4fe0f6a9f8d4589c730bd4ebb7a1c343ae62
Matthew Gharrity <mjg355@cornell.edu>
2017-02-05 18:57:34 -0500

    Update template

--- template.cpp
+++ template.cpp
@@ -6,24 +6,21 @@ using namespace std;
 
 #define TRACE(x) x
 #define WATCH(x) TRACE(cout << #x" = " << x << endl)
-#define WATCHR(b, e) TRACE(for (auto it = b; it != e; ++it) \
-    { if (it != b) cout << " "; cout << *it; } \
+#define WATCHR(b, e) TRACE( \
+    for (auto it = b; it != e; ++it) \
+        { if (it != b) cout << " "; cout << *it; } \
     cout << endl)
 #define WATCHC(v) TRACE({cout << #v" = "; WATCHR(v.begin(), v.end());})
 
 using ll = long long;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-using vi = vector<int>;
-using vvi = vector<vi>;
-using vb = vector<bool>;
-using vvb = vector<vb>;
 
 #define FU(i, a, b) for (auto i = (a); i < (b); ++i)
 #define fu(i, b) FU(i, 0, b)
 #define FD(i, a, b) for (auto i = (b) - 1; i >= (a); --i)
 #define fd(i, b) FD(i, 0, b)
 #define all(x) (x).begin(), (x).end()
+#define mp make_pair
+#define pb emplace_back
 
 int main() {
     ios_base::sync_with_stdio(false);

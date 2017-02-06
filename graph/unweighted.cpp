4ce7caea6719750e529333ca54d894810ae572ab
Matthew Gharrity <mjg355@cornell.edu>
2017-02-05 19:28:02 -0500

    Update unweighted graph; move to graph directory

--- graph/unweighted.cpp
+++ graph/unweighted.cpp
@@ -19,25 +19,26 @@
 using namespace std;
 
 struct graph {
-    const int null = -1;
     int n;
     vector<vector<int>> adj;
 
     graph(int n): n(n), adj(n) {}
 
+    vector<int>& operator[](size_t i) { return adj[i]; }
+
     void arc (int i, int j) { adj[i].emplace_back(j); }
     void edge(int i, int j) { arc(i, j), arc(j, i); }
 
     template <typename Black, typename Gray>
-    void dfs(int s, Black black, Gray gray) {
+    void dfs(int s, Black black, Gray gray) const {
         stack<pair<int,int>> q;
-        if (gray(null, s))
-            q.emplace(null, s);
+        q.emplace(-1, s);
         while (!q.empty()) {
             int x, c;
             tie(x, c) = q.top();
             q.pop();
-            black(x, c);
+            if (x != -1)
+                black(x, c);
             for (auto cc : adj[c])
                 if (gray(c, cc))
                     q.emplace(c, cc);
@@ -45,56 +46,65 @@ struct graph {
     }
 
     template <typename Black>
-    void dfsBlack(int s, Black black) {
+    void dfsBlack(int s, Black black) const {
         vector<int> seen(n);
-        return dfs(s, black, [&] (int x, int c) {
+        seen[s] = true;
+        dfs(s, black, [&](int x, int c) {
             return seen[c] ? false : seen[c] = true;
         });
     }
 
     template <typename Gray>
-    void dfsGray(int s, Gray gray) {
-        return dfs(s, [] (int x, int c) {}, gray);
+    void dfsGray(int s, Gray gray) const {
+        return dfs(s, [](int x, int c) {}, gray);
     }
 
-    int cc() {
+    int cc() const {
         int count = 0;
         vector<bool> seen(n);
-        auto mark = [&] (int x, int c) {
-            return seen[c] ? false : seen[c] = true;
-        };
-        for (int i = 0; i < n; ++i)
-            if (!seen[i])
-                dfsGray(i, mark), ++count;
+        for (int i = 0; i < n; ++i) {
+            if (!seen[i]) {
+                ++count;
+                seen[i] = true;
+                dfsGray(i, [&](int x, int c) {
+                    return seen[c] ? false : seen[c] = true;
+                });
+            }
+        }
         return count;
     }
 
     pair<bool, vector<bool>>
-    bipartite() {
+    bipartite() const {
         bool ok = true;
         vector<bool> color(n);
         vector<bool> seen(n);
-        auto mark = [&] (int x, int c) {
-            if (x == null) {
-                return seen[c] ? false : seen[c] = true;
-            } else if (seen[c]) {
-                ok &= color[c] != color[x];
-                return false;
-            } else {
-                seen[c] = true;
-                color[c] = !color[x];
-                return ok;
-            }
-        };
-        for (int i = 0; i < n; ++i)
-            dfsGray(i, mark);
+        for (int i = 0; i < n; ++i) {
+            dfsGray(i, [&](int x, int c) {
+                if (seen[c]) {
+                    ok &= color[c] != color[x];
+                    return false;
+                } else {
+                    seen[c] = true;
+                    color[c] = !color[x];
+                    return ok;
+                }
+            });
+        }
         return make_pair(ok, color);
+    }
+
+    graph rooted(int s) const {
+        graph g(n);
+        dfsBlack(s, [&](int x, int c) { g.arc(x, c); });
+        return g;
     }
 };
 
 int main() {
     graph g(7);
     g.edge(0, 1);
+    assert(g[0][0] == 1 && g[1][0] == 0);
     g.edge(0, 3);
     g.edge(1, 2);
     g.edge(2, 3);

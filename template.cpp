a5ded02f05c4f8df344a0d86b462d45de93da059
Matthew Gharrity <mjg355@cornell.edu>
2017-04-15 22:25:20 -0400

    Tweaks to template

--- template.cpp
+++ template.cpp
@@ -14,13 +14,13 @@ using namespace std;
 
 using ll = long long;
 
-#define FU(i, a, b) for (auto i = (a); i < (b); ++i)
+#define FU(i, a, b) for (int i = (a); i < (b); ++i)
 #define fu(i, b) FU(i, 0, b)
-#define FD(i, a, b) for (auto i = (b) - 1; i >= (a); --i)
+#define FD(i, a, b) for (int i = (int) (b) - 1; i >= (a); --i)
 #define fd(i, b) FD(i, 0, b)
 #define all(x) (x).begin(), (x).end()
 #define mp make_pair
-#define pb emplace_back
+#define pb push_back
 
 int main() {
     ios::sync_with_stdio(false);

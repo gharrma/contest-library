b15510f3f90d76875299c4e25ecb048280c38e92
Matthew Gharrity <gharrma@gmail.com>
2021-03-13 22:29:32 -0500

    Simplify seg_tree_delta

--- data-structures/seg_tree_delta.cpp
+++ data-structures/seg_tree_delta.cpp
@@ -1,11 +1,3 @@
-/*
- * Segment tree supporting delta updates.
- * Represented as a perfect binary tree, identity-initialized.
- * Root is 1. Parent of node i is i/2. Children of node i are 2*i and 2*i+1.
- * s := size (number of leaves)
- * v := underlying array
- * id := result identity
- */
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -13,74 +5,58 @@
 #include <cassert>
 using namespace std;
 
-template <typename M>
+// Like seg_tree, but supports incremental updates.
+// This is useful when the values are large objects (e.g. sets) that
+// can only change incrementally (e.g. adding a new element to a set).
+// When applying an update function f to an element, f must be commutative
+// with respect to the (implicit) binary operator for this segment tree.
+// That is, it must be true that f(a)*b = f(a*b).
+template <typename T>
 struct seg_tree_delta {
-    M m;
-    int s;
-    vector<typename M::State> v;
+    int n;
+    vector<T> v;
 
-    seg_tree_delta(int n, M m = M()): m(m) {
-        for (s = 1; s < n; )
-            s <<= 1;
-        v.resize(2*s);
-    }
+    seg_tree_delta(int n, T id): n(n), v(2*n, id) {}
 
-    void update(int i, typename M::Update u) {
-        for (i += s; i > 0; i /= 2) {
-            m.update(v[i], u);
+    // Apply f to a value and its O(lg n) ancestors.
+    template <typename F>
+    void update(int i, F f) {
+        assert(i <= i && i < n);
+        for (i += n; i > 0; i /= 2) {
+            f(v[i]);
         }
     }
 
-    typename M::Res query(int i, int j, typename M::Query q) {
-        i += s, j += s;
-        auto l = m.id, r = m.id;
-        for (; i <= j; i /= 2, j /= 2) {
-            if (i % 2 == 1) l = m.compose(l, m.query(v[i++], q));
-            if (j % 2 == 0) r = m.compose(m.query(v[j--], q), r);
+    // Visit the O(lg n) segments covering the range [l,r].
+    template <typename F>
+    void visit(int l, int r, F f) {
+        assert(0 <= l && r < n);
+        for (l += n, r += n; l <= r; l /= 2, r /= 2) {
+            if (l % 2 == 1) f(v[l++]);
+            if (r % 2 == 0) f(v[r--]);
         }
-        return m.compose(l, r);
-    }
-};
-
-struct M {
-    using State = unordered_map<int,int>;
-    using Update = int;
-    using Query = int;
-    using Res = int;
-    static constexpr Res id = 0;
-
-    static void update(State& state, Update val) {
-        ++state[val];
-    }
-
-    static Res query(State& state, Query val) {
-        return state[val];
-    }
-
-    static Res compose(Res a, Res b) {
-        return a + b;
     }
 };
 
 int main() {
     int n = 100;
     vector<unordered_map<int,int>> v(n);
-    seg_tree_delta<M> s(n);
+    seg_tree_delta s(n, unordered_map<int,int>());
     for (int t = 0; t < 1000000; ++t) {
         int val = rand() % 100;
         if (rand() % 2) {
             int i = rand() % n;
             ++v[i][val];
-            s.update(i, val);
+            s.update(i, [&](auto& m) { ++m[val]; });
         } else {
             int l = rand() % n;
             int r = rand() % n;
-            if (r < l)
-                swap(l, r);
+            if (r < l) swap(l, r);
+            int expected = 0;
+            for (int i = l; i <= r; ++i) expected += v[i][val];
             int res = 0;
-            for(int i = l; i <= r; ++i)
-                res += v[i][val];
-            assert(s.query(l, r, val) == res);
+            s.visit(l, r, [&](const auto& m) { if (m.count(val)) res += m.at(val); });
+            assert(res == expected);
         }
     }
     cout << "All tests passed" << endl;
